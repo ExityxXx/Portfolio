062984879726ae506fcfed34ccfe0c6455782125
exityx <gdlasorum@gmail.com>
2025-05-25 14:19:11 +0300

    Update and rename filter_alternative.cpp to filter.cpp

--- C++/filter.cpp
+++ C++/filter.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
-#include <string>
 
-bool is_even(int);
-bool is_positive(int);
-
-void action(bool(*)(int), int[], unsigned);
+void filter(bool(*)(int), int[], unsigned);
 
 int main() {
     int numbers[] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
     const unsigned n = std::size(numbers);
+
     std::cout << "Чётные числа:\n";
-    action(is_even, numbers, n);
+    filter([](int x){ return x % 2 == 0; }, numbers, n);
+
     std::cout << "Положительные числа:\n";
-    action(is_positive, numbers, n);
+    filter([](int a){ return a > 0; }, numbers, n);
 
     return 0;
 }
-bool is_even(int x) { return x % 2 == 0; }
-bool is_positive(int x) { return x > 0; }
 
-void action(bool(*func)(int), int array[], unsigned length) {
+void filter(bool(*func)(int), int array[], unsigned length) {
     for (int i = 0; i < length; ++i) {
         if (func(*(array + i))) {
             std::cout << *(array + i) << '\t';
