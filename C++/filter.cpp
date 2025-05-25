bcc2419f340a8d177007f6e6232e68a651b67804
exityx <gdlasorum@gmail.com>
2025-05-25 14:23:48 +0300

    Update filter.cpp

--- C++/filter.cpp
+++ C++/filter.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
+#include <vector>
 
-void filter(bool(*)(int), int[], unsigned);
+std::vector<int> filter(bool(*)(int), int[], unsigned);
 
 int main() {
     int numbers[] = {-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5};
     const unsigned n = std::size(numbers);
 
     std::cout << "Чётные числа:\n";
-    filter([](int x){ return x % 2 == 0; }, numbers, n);
+    std::vector<int> even_values = filter([](int x){ return x % 2 == 0; }, numbers, n);
 
-    std::cout << "Положительные числа:\n";
-    filter([](int a){ return a > 0; }, numbers, n);
+    for (const int &element : even_values) {
+        std::cout << element << '\t';
+    }
+    
+    std::cout << "\nПоложительные числа:\n";
+    std::vector<int> absolute_values = filter([](int a){ return a > 0; }, numbers, n);
 
+    for (const int &element : absolute_values) {
+        std::cout << element << '\t';
+    }
     return 0;
 }
 
-void filter(bool(*func)(int), int array[], unsigned length) {
+std::vector<int> filter(bool(*func)(int), int array[], unsigned length) {
+    std::vector<int> filtered_array;
+
     for (int i = 0; i < length; ++i) {
         if (func(*(array + i))) {
-            std::cout << *(array + i) << '\t';
+            filtered_array.push_back(*(array + i));
         }
     }
-    std::cout << std::endl;
+    return filtered_array;
 }
